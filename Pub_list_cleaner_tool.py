ef438548654c45938e34c8cd2a1ddf41703db1f9
markgreig <53443668+markgreig@users.noreply.github.com>
2024-03-01 14:40:10 +0000

    Update Pub_list_cleaner_tool.py

--- Pub_list_cleaner_tool.py
+++ Pub_list_cleaner_tool.py
@@ -13,6 +13,9 @@ def clean_publication_name(text):
     if text.startswith('www.'):
         return text
     
+    # Remove the contents within parentheses
+    text = re.sub(r"\(.*?\)", "", text)
+    
     text = re.sub(r"\..*$", "", text)
     text = re.sub(r"[^\w\s-]", "", text).lower()
     text = re.sub(r"\s", "", text)
